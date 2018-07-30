c36396ce71c0eb0e09946d2f7e1a60a1f0643184
Jordan <jordanmikeanderson@gmail.com>
2018-07-30 10:03:48 -0600

    Added course descriptions

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,20 +1,32 @@
 import React, { Component } from "react";
 
+import Icon from '../icon';
+import Arrow from "../arrow";
+import Action from "../action";
+
 class LibraryCourse extends Component {
-    render() {
-      return(
+  render() {
+    return (
         <div className="library-course">
-          <label className="library-course__title">Problem Solving</label>
-          { /* icon component */ }
-          { /* arrow component */ }
-          { /* action component */ }
-          <div className='library-course__description'>
+          <div className="library-course__title-check">
+            <label className="library-course__title">Problem Solving</label>
+            {Icon("fas fa-check", "library-course__icon")}
+          </div>
+          <Arrow className="library-course__arrow" />
+          <Action className="library-course__action" />
+          <div className="library-course__description">
             <label>Course Description</label>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fringilla facilisis mi, non pellentesque metus consectetur vel. Quisque dictum, lectus vitae dignissim tincidunt, sapien nibh placerat diam, quis blandit enim nulla in felis.</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+              fringilla facilisis mi, non pellentesque metus consectetur vel.
+              Quisque dictum, lectus vitae dignissim tincidunt, sapien nibh
+              placerat diam, quis blandit enim nulla in felis.
+            </p>
           </div>
         </div>
-      )
-    }
+
+    )
+  }
 }
 
 export default LibraryCourse;
