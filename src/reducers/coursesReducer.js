4af545359aec7d8499f745730b8fc995b688a52e
Jordan <jordanmikeanderson@gmail.com>
2018-08-01 11:46:58 -0600

    Finished functionality of adding and removing courses

--- src/reducers/coursesReducer.js
+++ src/reducers/coursesReducer.js
@@ -1,4 +1,7 @@
-import { FETCH_COURSES } from '../actions/types';
+import {
+  FETCH_COURSES,
+  TOGGLED_ENROLLED
+} from '../actions/types';
 
 export default function(state = [], action) {
   switch (action.type) {
@@ -7,6 +10,18 @@ export default function(state = [], action) {
       return [
           ...state,
           ...action.payload
+      ]
+    case TOGGLED_ENROLLED:
+    console.log(action.payload)
+    const newCourses = state.map(course => {
+      if(course.id == action.payload) {
+        course.enrolled = !course.enrolled
+      }
+      return course
+    })
+
+      return [
+        ...newCourses
       ]
     default:
       return state;
